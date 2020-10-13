3568f5a74e15d307335bf30c13060d5104cf6946
Sofia Rodrigues <sofiarodrigues@MacBook-Pro-de-Sofia-3.local>
2020-10-13 18:00:19 +0100

    novas classes: Cylinder and Cube

---
+++ js/cube.js
@@ -0,0 +1,16 @@
+class Cube extends THREE.Object3D{
+
+    constructor(obj, x, y, z, rad, len, height, color){
+
+        super();
+
+        var material = new THREE.MeshBasicMaterial({ color: color, wireframe: true });
+        var geometry = new THREE.BoxGeometry(len, height, len);
+
+        mesh = new THREE.Mesh(geometry, material);
+        mesh.rotation.set(0, 0, Math.PI*rad);
+        mesh.position.set(x, y, z);
+
+    }
+
+}

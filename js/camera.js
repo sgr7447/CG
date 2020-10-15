a81aeb7eb585270bda82af584da616c3fd041e3c
Daniela Regina <57944315+danielaregina@users.noreply.github.com>
2020-10-15 16:00:29 +0100

    Add files via upload

--- js/camera.js
+++ js/camera.js
@@ -2,8 +2,8 @@ class Camera extends THREE.OrthographicCamera{
 
     constructor(){
 
-        var aspect = window.innerWidth / window.innerHeight;
-        var aspectRatio = (aspect * 3);
+        var aspect = window.innerHeight / window.innerWidth;
+        var aspectRatio = (aspect * 13);
         var width = window.innerWidth / aspectRatio;
         var height = window.innerHeight / aspectRatio;
         super(-width/2, width/2, height/2, -height/2, 1, 1000); //default
@@ -16,7 +16,7 @@ class Camera extends THREE.OrthographicCamera{
 
         this.num = 1;
         this.onResize();
-        this.position.set(0, 0, 500);
+        this.position.set(0, 0, window.innerHeight);
         this.lookAt(scene.position);
 
     }
@@ -26,7 +26,7 @@ class Camera extends THREE.OrthographicCamera{
 
         this.num = 2;
         this.onResize();
-        this.position.set(0, -window.innerHeight/2, 0);
+        this.position.set(0, window.innerHeight, 0);
         this.lookAt(scene.position);
     }
 
@@ -35,29 +35,11 @@ class Camera extends THREE.OrthographicCamera{
 
         this.num = 3;
         this.onResize();
-        this.position.set(-window.innerHeight/2, 0, 0);
+        this.position.set(-window.innerHeight, 0, 0);
         this.lookAt(scene.position);
     }
 
     onResize() {
-
         renderer.setSize(window.innerWidth, window.innerHeight);
-
-        if (window.innerHeight > 0 && window.innerWidth > 0) {
-
-            this.aspect = window.innerWidth / window.innerHeight;
-            var aspectRatio = (this.aspect * 3);
-            var width = window.innerWidth / aspectRatio;
-            var height = window.innerHeight / aspectRatio;
-
-            this.left = -width/2;
-            this.right = width/2
-            this.top = height/2;
-            this.bottom = -height/2;
-            this.updateProjectionMatrix();
-        }
-
     }
-
-
 }

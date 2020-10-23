c5fa337157ea84b8450ce9421d69e5c402acb048
danielaregina <danielaregina@live.com.pt>
2020-10-23 22:25:47 +0100

    dei o meu melhor meninas

--- proj2/js/ball.js
+++ proj2/js/ball.js
@@ -31,14 +31,21 @@ class Ball extends Sphere{
         this.translateZ(this.direction.z * this.delta * this.start_speed);
 
         if (this.start_speed > 0) {
+
+            //mov unif retardado
             this.start_speed = this.start_speed - this.delta*10;
         }
+
         else {
             this.start_speed = 0;
         }
     }
 
     spin(){
+
+        if (this.start_speed>0 || this.fall_speed>0){
+            
+        }
 
     }
 

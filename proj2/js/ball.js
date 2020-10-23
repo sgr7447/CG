6c5fe7174d69c05d91dd83deabf1903f19094311
danielaregina <danielaregina@live.com.pt>
2020-10-23 09:34:26 +0100

    correcoes

--- proj2/js/ball.js
+++ proj2/js/ball.js
@@ -3,8 +3,9 @@ class Ball extends Sphere{
     constructor(){
 
         var radius = 5;
-        var x = [-100 +radius, 100 -radius];
-        var z = [-50 +radius, 50 -radius];
+        var hole_radius = 14;
+        var x = [-100 +radius + hole_radius, 100 -radius -hole_radius];
+        var z = [-50 +radius + hole_radius, 50 -radius -hole_radius];
 
         super(THREE.Math.randFloat(x[0], x[1]), radius +0.1, THREE.Math.randFloat(z[0], z[1]), radius, 0xFFFFFFF);
 

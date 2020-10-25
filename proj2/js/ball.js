04f1d60cf16835749b215fb63b5c91c84677c0b4
Sofia Rodrigues <sofiarodrigues@MacBook-Pro-de-Sofia-3.local>
2020-10-25 01:02:26 +0100

    moveinsidehole loading

--- proj2/js/ball.js
+++ proj2/js/ball.js
@@ -17,23 +17,27 @@ class Ball extends Sphere{
         var random_z = THREE.Math.randInt(-1,1);
         this.direction = new THREE.Vector3(random_x, 0, random_z);
         this.direction.setLength(this.start_speed);
-
         //FLAGS
         this.fall = false;
         this.inside_hole = false;
+
+        this.radius = radius;
+
 
         scene.add(this);
 
     }
 
     move(){
-
+        if(this.fall) return;
         this.translateX(this.direction.x * this.delta);
         this.translateZ(this.direction.z * this.delta);
 
         if (this.direction.length() > 0) {
-            this.direction.setLength(this.direction.length() - this.delta*10);
+            var l = this.direction.length()
+            this.direction.setLength(l - this.delta*10);
             //this.spin();
+
         }
         else {
             this.direction.setLength(0);
@@ -55,12 +59,13 @@ class Ball extends Sphere{
 
     falling(){
 
-        if (this.fall){
+      if (this.fall){
+          console.log(this.direction.length());
+          this.direction.set(0, this.direction.length() + this.delta * 9.8, 0);
+          this.translateY(-this.direction.length() * this.delta);
 
-            this.direction.set(0, this.direction.length() + this.delta * 9.8, 0);
-            this.translateY(-this.direction.length() * this.delta);
+      }
 
-        }
     }
 
 
@@ -72,11 +77,15 @@ class Ball extends Sphere{
         if (this.inside_hole && !this.fall){
 
             //verifica se esta dentro do buraco
-            if (Math.abs(this.hole_x - this.position.x) + this.radius < this.hole_radius &&
-                Math.abs(this.hole_z - this.position.z) + this.radius < this.hole_radius){
+            
+            var distHoleToBall = Math.sqrt((hole_x - this.position.x)**2 + (hole_z - this.position.z)**2);
+
+            if (distHoleToBall + this.radius < hole_radius){
 
                     //encontra-se pronto para cair
+
                     this.fall = true;
+                    this.direction.set(0, 1, 0);
 
             }
 
@@ -100,7 +109,7 @@ class Ball extends Sphere{
             this.hole_z = hole_z;
             this.hole_radius = hole_radius;
             var speed = this.direction.length();
-            this.direction.set(hole_x - this.position.x, 0, hole_z - this.position.z).normalize(); //normalizar o vetor;
+            this.direction.set(hole_x - this.position.x, this.direction.y, hole_z - this.position.z).normalize(); //normalizar o vetor;
             this.direction.setLength(speed);
 
             return true;
@@ -112,35 +121,32 @@ class Ball extends Sphere{
     // se o centro da bola estiver dentro do buraco cai,
     // se nao mantem-se no sitio
     checkInHole(){
+        if(this.inside_hole) return;
+        var hole_radius = 6.5;
 
-        if (!this.fall){
+        var hole_x = -100+6.5+1;
+        var hole_z = -50+6.5+1;
+        if (this.insideHole(hole_x, hole_z, hole_radius)) return;
 
-            var hole_radius = 6.5;
+        hole_x = -100+6.5+1;
+        hole_z = 50-6.5-1;
+        if (this.insideHole(hole_x, hole_z, hole_radius)) return;
 
-            var hole_x = -100+6.5+1;
-            var hole_z = -50+6.5+1;
-            if (this.insideHole(hole_x, hole_z, hole_radius)) return;
+        hole_x = 0;
+        hole_z = -50+6.5+1;
+        if(this.insideHole(hole_x, hole_z, hole_radius)) return;
 
-            hole_x = -100+6.5+1;
-            hole_z = 50-6.5-1;
-            if (this.insideHole(hole_x, hole_z, hole_radius)) return;
+        hole_x = 100-6.5-1;
+        hole_z = -50+6.5+1;
+        if (this.insideHole(hole_x, hole_z, hole_radius)) return;
 
-            hole_x = 0;
-            hole_z = -50+6.5+1;
-            if(this.insideHole(hole_x, hole_z, hole_radius)) return;
+        hole_x = 100-6.5-1;
+        hole_z = 50-6.5-1;
+        if (this.insideHole(hole_x, hole_z, hole_radius)) return;
 
-            hole_x = 100-6.5-1;
-            hole_z = -50+6.5+1;
-            if (this.insideHole(hole_x, hole_z, hole_radius)) return;
-
-            hole_x = 100-6.5-1;
-            hole_z = 50-6.5-1;
-            if (this.insideHole(hole_x, hole_z, hole_radius)) return;
-
-            hole_x = 0;
-            hole_z = 50-6.5-1;
-            if (this.insideHole(hole_x, hole_z, hole_radius)) return;
-        }
+        hole_x = 0;
+        hole_z = 50-6.5-1;
+        if (this.insideHole(hole_x, hole_z, hole_radius)) return;
 
     }
 
@@ -150,15 +156,12 @@ class Ball extends Sphere{
         var wallU = 50-0.05;
         var wallD = -50+0.05;
 
-        if (!this.inside_hole){
+        if((this.position.x <= wallL + this.radius) || (this.position.x >=wallR - this.radius)){
+          this.direction.x = -this.direction.x;
+        }
 
-            if((this.position.x <= wallL + this.radius) || (this.position.x >=wallR - this.radius)){
-              this.direction.x = -this.direction.x;
-            }
-
-            if((this.position.z >= wallU - this.radius) || (this.position.z <=wallD + this.radius)){
-              this.direction.z = -this.direction.z;
-            }
+        if((this.position.z >= wallU - this.radius) || (this.position.z <=wallD + this.radius)){
+          this.direction.z = -this.direction.z;
         }
     }
 
@@ -168,11 +171,12 @@ class Ball extends Sphere{
 
     update(){
         this.updateTime();
-        this.checkInHole();
-        this.moveInsideHole();
         this.wallColision();
+        this.checkInHole();
         this.move();
+        this.moveInsideHole();
         this.falling();
+
 
     }
 

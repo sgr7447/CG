afd9aedf00536e1226922ddd58610158c460bfd0
carolina2300 <carolinanmarcal@gmail.com>
2020-10-28 11:49:13 +0000

    bolas criadas nao origem (colam-se menos)

--- proj2/js/ballsHandler.js
+++ proj2/js/ballsHandler.js
@@ -1,187 +1,245 @@
-class BallsHandler {
+class Ball extends Sphere{
 
-    constructor(){
+    constructor(radius, color){
 
-        this.balls = [];
-        this.ghostBalls = [];
-        this.radius = 5;
-        this.ghostSpeed = 80;
+        super(0, 0, 0, radius, color);
 
-        //ballCamera = new BallCamera();
+        //TIME
+        this.time = new THREE.Clock();
+        this.delta;
 
-        //NORMAL BALLS
-        //amarelo, azul, verde, rosa, roxo, vermelho
-        var color_array = ['#FFE4B5', '#87CEEB', '#8FBC8F', '#DB7093', '#9370DB', '#FF6347'];
+        //SPEED
+        this.start_speed = THREE.Math.randFloat(50,80);
+        this.fall_speed = 0;
 
-        while(this.balls.length < 15){
+        //DIRECTION
+        var random_x = THREE.Math.randInt(-1,1);
+        var random_z = THREE.Math.randInt(-1,1);
+        this.direction = new THREE.Vector3(random_x, 0, random_z);
+        this.direction.setLength(this.start_speed);
 
-            var hole_radius = 14;
+        //FLAGS
+        this.fall = false;
+        this.inside_hole = false;
+        this.wall_colision_x = false;
+        this.wall_colision_z = false;
 
-            var x = [-100 + this.radius + hole_radius, 100 -this.radius -hole_radius];
-            var z = [-50 + this.radius + hole_radius, 50 -this.radius -hole_radius];
-            var x_position = THREE.Math.randFloat(x[0], x[1]);
-            var z_position = THREE.Math.randFloat(z[0], z[1]);
+        this.radius = radius;
 
-            var flag = this.verifyBalls(x_position, z_position, this.radius);
+        this.hole_x;
+        this.hole_z;
+        this.hole_radius;
 
-            if (flag){
-                var index_color = THREE.Math.randInt(0,4);
-                var color = color_array[index_color];
-                
-                var ball = new Ball(x_position, z_position, this.radius, color);
-                this.balls.push(ball);
+        scene.add(this);
+
+    }
+
+    setBall(x,y,z){
+        this.position.x = x;
+        this.position.y = y;
+        this.position.z = z;
+    }
+
+    move(){
+        if(this.fall) return;
+        this.spin();
+        this.position.x += (this.direction.x * this.delta);
+        this.position.z += (this.direction.z * this.delta);
+
+        if (this.direction.length() > 0) {
+            var l = this.direction.length()
+            this.direction.setLength(l - this.delta*10);
+        }
+        else {
+            this.direction.setLength(0);
+        }
+    }
+
+    spin(){
+        if (!this.fall && this.direction.length() != 0){
+            var rotationAxis = new THREE.Vector3(this.direction.x, 0, this.direction.z); //vetor perpendicular ao da velocidade
+            rotationAxis.cross(THREE.Object3D.DefaultUp).normalize(); //produto externo
+
+            var auxVector = new THREE.Vector3(this.direction.x * this.delta, 0, this.direction.z * this.delta);
+            var angle = -auxVector.length() / this.radius;
+            this.rotateOnWorldAxis(rotationAxis, angle);
+        }
+    }
+
+    falling(){
+
+      if (this.fall){
+          this.direction.set(0, this.direction.length() + this.delta * 9.8, 0);
+              this.position.y -= (this.direction.length() * this.delta);
+
+      }
+
+    }
+
+
+    //quando o centro de massa da bola esta dentro do buraco mas
+    //uma parte da bola ainda esta na mesa
+    moveInsideHole(){
+
+        //esta dentro do buraco mas nao esta pronto para cair
+        if (this.inside_hole && !this.fall){
+
+            //verifica se esta dentro do buraco
+
+            var distHoleToBall = Math.sqrt((this.hole_x - this.position.x)**2 + (this.hole_z - this.position.z)**2);
+
+            if (distHoleToBall + this.radius < this.hole_radius){
+
+                    //encontra-se pronto para cair
+
+                    this.fall = true;
+                    this.direction.set(0, 1, 0);
 
             }
 
+            //se nao estiver continuar move com velocidade inicial, mas mudamos a direcao no inside_hole
+
         }
-
-        //GHOST BALLS
-
-        //club0
-        this.createGhostBall(0);
-
-        //club1
-        this.createGhostBall(1);
-
-        //club2
-        this.createGhostBall(2);
-
-        //club3
-        this.createGhostBall(3);
-
-        //club4
-        this.createGhostBall(4);
-
-        //club5
-        this.createGhostBall(5);
 
     }
 
-    createGhostBall(index){
+    insideHole(hole_x, hole_z, hole_radius){
 
-        var x=0, z=0;
-        var direction = new THREE.Vector3(0,0,0);
+        var distHoleToBall = Math.sqrt((hole_x - this.position.x)**2 + (hole_z - this.position.z)**2);
 
-        if (index == 0){
-            x = -100+6.5+1;
-            direction.set(1,0,0);
+        if ( distHoleToBall < hole_radius) {
+
+            //var color = new THREE.Color('#ff0000');
+            //this.material.color.setHex( color.getHex());
+
+            this.inside_hole = true;
+            this.hole_x = hole_x;
+            this.hole_z = hole_z;
+            this.hole_radius = hole_radius;
+            var speed = this.direction.length();
+            this.direction.set(hole_x - this.position.x, this.direction.y, hole_z - this.position.z).normalize(); //normalizar o vetor;
+            this.direction.setLength(speed);
+
+            return true;
         }
 
-        else if(index == 1){
-            x = -50;
-            z = -(50-6.5-1);
-            direction.set(0,0,1);
-        }
-
-        else if(index == 2){
-            x = 50;
-            z = -(50-6.5-1)
-            direction.set(0,0,1);
-        }
-
-        else if(index == 3){
-            x = 100-6.5;
-            direction.set(-1,0,0);
-        }
-
-        else if(index == 4){
-            x = 50;
-            z = 50-6.5-1;
-            direction.set(0,0,-1);
-        }
-
-        else if(index == 5){
-            x = -50;
-            z = 50-6.5-1;
-            direction.set(0,0,-1);
-        }
-        var ghostBall = new Ball(x, z, this.radius, '#FFFFFF');
-        ghostBall.direction = direction;
-        ghostBall.direction.setLength(this.ghostSpeed);
-        this.ghostBalls[index] = ghostBall;
+        return false;
     }
 
-    shoot(index, clubs){
+    // se o centro da bola estiver dentro do buraco cai,
+    // se nao mantem-se no sitio
+    checkInHole(){
+        if(this.inside_hole) return;
+        var hole_radius = 6.5;
 
-        if (index >= 0){
-          var x,z;
-          var pos_x = clubs[index].position.x;
-          var pos_z = clubs[index].position.z;
+        var hole_x = -100+6.5+1;
+        var hole_z = -50+6.5+1;
+        if (this.insideHole(hole_x, hole_z, hole_radius)) return;
 
-          if(pos_z != 0){
-            x = -Math.sin(clubs[index].rotation.y);
-            z = Math.cos(clubs[index].rotation.y);
-            if(clubs[index].position.z < 0) x=-x;
-            if(clubs[index].position.z > 0) z=-z;
-          }
+        hole_x = -100+6.5+1;
+        hole_z = 50-6.5-1;
+        if (this.insideHole(hole_x, hole_z, hole_radius)) return;
 
-          else{
-            z = Math.sin(clubs[index].rotation.y);
-            x = Math.cos(clubs[index].rotation.y);
-            if(index == 0) z=-z;
-            if(index == 3) x=-x;
-          }
+        hole_x = 0;
+        hole_z = -50+6.5+1;
+        if(this.insideHole(hole_x, hole_z, hole_radius)) return;
 
-          this.ghostBalls[index].direction.set(x, 0, z);
-          this.ghostBalls[index].direction.setLength(this.ghostSpeed);
-          this.balls.push(this.ghostBalls[index]);
-          this.createGhostBall(index);
-      }
+        hole_x = 100-6.5-1;
+        hole_z = -50+6.5+1;
+        if (this.insideHole(hole_x, hole_z, hole_radius)) return;
+
+        hole_x = 100-6.5-1;
+        hole_z = 50-6.5-1;
+        if (this.insideHole(hole_x, hole_z, hole_radius)) return;
+
+        hole_x = 0;
+        hole_z = 50-6.5-1;
+        if (this.insideHole(hole_x, hole_z, hole_radius)) return;
+
     }
 
-    verifyBalls(x, z, radius){
-        var flag = true
-        if (this.balls.length == 0){return flag;}
-        for (var i = 0; i < this.balls.length; i++) {
+    checkWallColision(){
+        var wallL = -100+0.05;
+        var wallR = 100-0.05;
+        var wallU = 50-0.05;
+        var wallD = -50+0.05;
 
-          var ball = this.balls[i];
-          var dist_balls = Math.sqrt((x - ball.position.x)**2 + (z - ball.position.z)**2);
-          if (dist_balls < 2*radius){
-              flag = false;
-              break;
+        if((this.position.x <= wallL + this.radius) || (this.position.x >=wallR - this.radius)){
+          this.wall_colision_x = true;
         }
-      }
-      return flag;
+
+        if((this.position.z >= wallU - this.radius) || (this.position.z <=wallD + this.radius)){
+          this.wall_colision_z = true;
+        }
     }
 
-    checkBallColision(ball_index){
+    treatWallColision(){
 
-        var ball1 = this.balls[ball_index];
-        var list_ball_colisions = [];
+        if (this.wall_colision_x){
+            this.direction.x = -this.direction.x;
+            this.wall_colision_x = false;
+        }
 
-        for (var j = ball_index+1; j < this.balls.length; j++) {
+        if (this.wall_colision_z){
+            this.direction.z = -this.direction.z;
+            this.wall_colision_z = false;
+        }
+    }
 
-            if (this.balls[j]){
-                var ball2 = this.balls[j];
+    treatBallsColision(list_ball_colisions){
 
-                var distBalls = Math.sqrt((ball1.position.x - ball2.position.x)**2 + (ball1.position.z - ball2.position.z)**2);
+        var ball1 = this;
 
-                if(distBalls <= 2*ball1.radius){
-                    list_ball_colisions.push(ball2);
+        for (var i=0; i<list_ball_colisions.length; i++){
 
-                }
+            var ball2 = list_ball_colisions[i];
+
+            //verifica se as bolas estão na mesma posição nos yy
+            if(ball1.direction.y == ball2.direction.y){
+
+                var diffVelocity1 = ball1.direction.clone().sub(ball2.direction);
+                var diffVelocity2 = ball2.direction.clone().sub(ball1.direction);
+
+                var diffPosition1 = ball1.position.clone().sub(ball2.position);
+                var diffPosition2 = ball2.position.clone().sub(ball1.position);
+
+
+                var dot1 = diffVelocity1.clone().dot(diffPosition1);
+                var dot2 = diffVelocity2.clone().dot(diffPosition2);
+
+                //norma ao quadrado
+                var norm1 = diffPosition1.lengthSq();
+                var norm2 = diffPosition2.lengthSq();
+
+                var tangent = new THREE.Vector3(-diffPosition1.z, 0 ,-diffPosition1.x);
+
+                var dir1 = ball1.direction.clone().sub(diffPosition1.multiplyScalar(dot1 / norm1));
+                var dir2 = ball2.direction.clone().sub(diffPosition2.multiplyScalar(dot2 / norm2));
+
+                ball1.direction = dir1;
+                ball2.direction = dir2;
             }
         }
+    }
 
-        return list_ball_colisions;
+    updateTime() {
+        this.delta = this.time.getDelta();
     }
 
     update(){
 
-        for (var i=0; i<this.balls.length; i++){
+        this.updateTime();
 
-            if (this.balls[i]){
-                //does movement and checks for hole and wall colisions
-                this.balls[i].update();
+        //movement
+        this.move();
+        this.falling();
 
-                //checks for ball colisions
-                var list_ball_colisions = this.checkBallColision(i);
+        //checks for inside hole colision
+        this.checkInHole();
 
-                //treating all colisions
-                this.balls[i].moveInsideHole();
-                this.balls[i].treatWallColision();
-                this.balls[i].treatBallsColision(list_ball_colisions);
-            }
-        }
+        //checks for wall colision
+        this.checkWallColision();
+
     }
+
 }

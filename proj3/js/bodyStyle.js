9e49b4fb9247a011339e45850415f651bfa18e4e
Sofia Rodrigues <sofiarodrigues@MacBook-Pro-de-Sofia-3.local>
2020-11-07 18:41:16 +0000

    vertices da parte de baixo

--- proj3/js/bodyStyle.js
+++ proj3/js/bodyStyle.js
@@ -8,20 +8,21 @@ class BodyStyle extends THREE.Object3D{
         var bodyStyle = new THREE.Geometry();
 
         var platformHeight = 60;
+        var wheelRadius = 88.9;
 
         var bottomBodyStyleLength = 588.5;
         var bottomBodyStyleWidth = 202.7;
         var bodyStyleHeight = 190.5;
 
         bodyStyle.vertices.push(
-        	new THREE.Vector3( -bottomBodyStyleLength/2, platformHeight + 190.5/2 , -bottomBodyStyleWidth/2),
-        	new THREE.Vector3( bottomBodyStyleLength/2, platformHeight + 190.5/2 , -bottomBodyStyleWidth/2),
-        	new THREE.Vector3( bottomBodyStyleLength/2, platformHeight + 190.5*3/2 , -bottomBodyStyleWidth/2),
-            new THREE.Vector3( -bottomBodyStyleLength/2, platformHeight + 190.5*3/2 , -bottomBodyStyleWidth/2),
-            new THREE.Vector3( -bottomBodyStyleLength/2, platformHeight + 190.5/2 , bottomBodyStyleWidth/2),
-        	new THREE.Vector3( bottomBodyStyleLength/2, platformHeight + 190.5/2 , bottomBodyStyleWidth/2),
-        	new THREE.Vector3( bottomBodyStyleLength/2, platformHeight + 190.5*3/2 , bottomBodyStyleWidth/2),
-            new THREE.Vector3( -bottomBodyStyleLength/2, platformHeight + 190.5*3/2 , bottomBodyStyleWidth/2),
+        	new THREE.Vector3( -bottomBodyStyleLength/2, platformHeight + wheelRadius, -bottomBodyStyleWidth/2),
+        	new THREE.Vector3( bottomBodyStyleLength/2, platformHeight + wheelRadius, -bottomBodyStyleWidth/2),
+        	new THREE.Vector3( bottomBodyStyleLength/2, platformHeight + wheelRadius + bodyStyleHeight/2, -bottomBodyStyleWidth/2),
+            new THREE.Vector3( -bottomBodyStyleLength/2, platformHeight + wheelRadius + bodyStyleHeight/2, -bottomBodyStyleWidth/2),
+            new THREE.Vector3( -bottomBodyStyleLength/2, platformHeight + wheelRadius, bottomBodyStyleWidth/2),
+        	new THREE.Vector3( bottomBodyStyleLength/2, platformHeight + wheelRadius, bottomBodyStyleWidth/2),
+        	new THREE.Vector3( bottomBodyStyleLength/2, platformHeight + wheelRadius + bodyStyleHeight/2, bottomBodyStyleWidth/2),
+            new THREE.Vector3( -bottomBodyStyleLength/2, platformHeight + wheelRadius + bodyStyleHeight/2 bottomBodyStyleWidth/2),
         );
 
         bodyStyle.faces.push(new THREE.Face3( 0, 1, 2));
@@ -33,6 +34,8 @@ class BodyStyle extends THREE.Object3D{
         mesh.position.set(0, 0, 0);
 
         this.add(mesh);
+
+        bodyStyle.scale(0.2, 0.2, 0.2);
 
         scene.add(bodyStyle);
 

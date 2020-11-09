01bcc3045ed7adf39bcc5e3b5fd0d265f713e37f
danielaregina <danielaregina@live.com.pt>
2020-11-09 23:07:56 +0000

    floor and platform

--- proj3/js/platform.js
+++ proj3/js/platform.js
@@ -4,7 +4,7 @@ class Platform extends THREE.Object3D{
 
         super();
 
-        var platformColor = '#4682B4';
+        var platformColor = '#D2B48C';
         var platformRadius = 350;
 
         this.add(new Cylinder(0, platformHeight/2, 0, 0, 0, 0, platformHeight, platformRadius, 200, 10, platformColor));

253301456b5c20d91fc11f3fca931a5ca12eab90
carolina2300 <carolinanmarcal@gmail.com>
2020-11-10 01:26:29 +0000

    total fixed car lights (ja nao estao sobrepostas a carroçaria)

--- proj3/js/truckLights.js
+++ proj3/js/truckLights.js
@@ -29,6 +29,13 @@ class TruckLights extends THREE.Object3D{
         var vertex21 = new THREE.Vector3();
         var translationF = new THREE.Vector3(0, -6, 0);
         var translationT = new THREE.Vector3(0, -6, 0);
+        var vec12_18 = new THREE.Vector3();
+        var vec13_19 = new THREE.Vector3();
+        var vec8_16 = new THREE.Vector3();
+        var vec9_17 = new THREE.Vector3();
+        var normalF = new THREE.Vector3();
+        var normalL = new THREE.Vector3();
+        var normalR = new THREE.Vector3();
 
         vertex8 = bodyStyle.bodyStyle.vertices[8].clone();
         vertex9 = bodyStyle.bodyStyle.vertices[9].clone();
@@ -48,6 +55,25 @@ class TruckLights extends THREE.Object3D{
         vertex17.add(translationF);
         vertex18.add(translationF);
         vertex19.add(translationF);
+
+        vec12_18.subVectors(vertex18, vertex12);
+        vec13_19.subVectors(vertex19, vertex13);
+        vec8_16.subVectors(vertex16, vertex8);
+        vec9_17.subVectors(vertex17, vertex9);
+
+        normalF.crossVectors(vec13_19, vec8_16).normalize();
+        normalL.crossVectors(vec13_19, vec12_18).normalize();
+        normalR.crossVectors(vec8_16, vec9_17).normalize();
+
+        vertex8.add(normalF);
+        vertex9.add(normalF);
+        vertex12.sub(normalL);
+        vertex13.add(normalF);
+        vertex16.add(normalF);
+        vertex17.add(normalR);
+        vertex18.sub(normalL);
+        vertex19.add(normalF);
+
 
         vertex20.add(translationT);
         vertex21.add(translationT);
